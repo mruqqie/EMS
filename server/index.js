f9c3d8e15bacfbfa856224a6095a8d23673bb256
mruqqie <mruqqie@gmail.com>
2023-02-28 12:11:14 +0100

    deleted:    index.js 	new file:   server/index.js 	modified:   server/package-lock.json 	modified:   server/package.json

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("connected")
+    server
+}).catch((err) => {
+    console.log({err})
+    process.exit(1)
+});
