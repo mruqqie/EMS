4a5e18dfe75a8ffa726af2ecd3556cee2d3002e9
mruqqie <mruqqie@gmail.com>
2023-02-28 15:19:37 +0100

    modified:   server/index.js 	new file:   server/src/models/model.options.js

--- server/index.js
+++ server/index.js
@@ -6,19 +6,22 @@ import mongoose from "mongoose";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT
+const port = process.env.PORT;
 const server = app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
-
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("connected")
-    server
-}).catch((err) => {
-    console.log({err})
-    process.exit(1)
+	console.log(`Listening on port ${port}`);
 });
+
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then(() => {
+		console.log("connected");
+		server;
+	})
+	.catch((err) => {
+		console.log({ err });
+		process.exit(1);
+	});
